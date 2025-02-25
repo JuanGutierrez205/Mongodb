6f196034401bdfd7a11c085f00a5b2e71e2a600e
JuanGutierrez205 <jdgutierrezm@unicauca.edu.co>
2025-02-24 20:36:32 -0500

    Subiendo proyecto a nuevo repositorio

--- src/db.js
+++ src/db.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
-
-
 const DB_USER = "admin"; // Usuario de MongoDB
-const DB_PASSWORD = "vagrant"; // Contraseña del usuario
-const DB_HOST = "selene.unicauca.edu.co"; // O IP del servidor MongoDB
-const DB_PORT = "8004"; // Puerto en el que corre MongoDB
+const DB_PASSWORD = "password123"; // Contraseña del usuario
+const DB_HOST = "192.168.20.209"; // O IP del servidor MongoDB
+const DB_PORT = "28017"; // Puerto en el que corre MongoDB
 const DB_NAME = "appdb"; // Nombre de la base de datos
 
 const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
